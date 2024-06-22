fb36deba171bbc26366b776953cf4ae2cf5c3111
Nicholas W <sirnicson@example.com>
2024-06-22 21:14:11 +0100

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/**                                                                     
+/**                                                                    
  * main - Entry point of the program
- *                                                                      
+ *                                                                     
  * Description: Prints a string to the standard output
  * Return: Always 0 (Success)
  */                                                                   
@@ -10,4 +10,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
