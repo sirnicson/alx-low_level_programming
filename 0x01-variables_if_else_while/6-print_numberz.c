87d12a04bf5c96184ca64e1bc3f93922cf21999c
Nicholas W <sirnicson@example.com>
2024-06-23 09:30:12 +0100

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,27 +12,12 @@
 int main(void)
 {
 	int n; /* Declare variable n of type int */
+
 	for (n = 0; n < 10; n++) /* Sets n to 0, increases to 9 */
 	{
 		putchar(n + '0'); /* Prints n to equate ASCII xter*/
 	}
+
 	putchar('\n'); /* Print new line after loop*/
 	return (0);
 }
-
-/**
- * Character Conversion: 
- * To print using putchar, convert integers
- * to their corresponding ASCII characters
- * by adding the ASCII value of '0'.
- * Character '0': ASCII value is 48.
- * Character '1': ASCII value is 49.
- * Character '2': ASCII value is 50.
- * Character '3': ASCII value is 51.
- * Character '4': ASCII value is 52.
- * Character '5': ASCII value is 53.
- * Character '6': ASCII value is 54.
- * Character '7': ASCII value is 55.
- * Character '8': ASCII value is 56.
- * Character '9': ASCII value is 57.
- */
