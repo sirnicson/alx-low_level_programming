803ed621b77405cb2d1a4d1d1780aa1f581e31e1
Nicholas W <sirnicson@example.com>
2024-06-23 15:13:00 +0100

    Software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,34 +13,35 @@
 int main(void)
 {
 	int t1, u1, t2, u2;
-	
+
 	for (t1 = 0; t1 < 10; t1++) /* Loop for tens digit of first number */
 	{
 		for (u1 = 0; u1 < 10; u1++) /* Loop for unit digit of first number */
 		{
-			for (t2 = t1; t2 < 10; t2++) /* Loop for tens digit of second number, starts from t1 */
+			for (t2 = t1; t2 < 10; t2++) /* Loop t2, starts from t1 */
 			{
 				/* Start u2 from 0 if it's the same tens digit, otherwise start from u1 */
 				int start_u2 = (t1 == t2) ? u1 : 0;
-				
-				for (u2 = start_u2; u2 < 10; u2++) /* Loop for ones digit of second number */
+
+				for (u2 = start_u2; u2 < 10; u2++) /* Loop for u2 */
 				{
 					/* Skip printing the same number (e.g., 00 00) */
 					if (t1 == t2 && u1 == u2)
 						continue;
-					
+
 					/* Print the first number */
 					putchar(t1 + '0');
 					putchar(u1 + '0');
-					
+
 					/* Print the second number */
 					putchar(' ');
 					putchar(t2 + '0');
 					putchar(u2 + '0');
-					
+
 					/* Check if it's not the last combination */
 					if (!(t1 == 9 && u1 == 8 && t2 == 9 && u2 == 9))
 					{
+
 						/* Print comma and space */
 						putchar(',');
 						putchar(' ');
