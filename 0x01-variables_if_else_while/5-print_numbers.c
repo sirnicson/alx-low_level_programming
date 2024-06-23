72f01fbd847094cba24570adabc5679357dd1685
Nicholas W <sirnicson@example.com>
2024-06-23 08:54:04 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h> /* i-o library for printing */
+
+/**
+ * main - Entry point of program
+ * ...
+ * Description: Prints all single digits of base 10
+ * Starting from 0
+ * Followed by a newline
+ * ...
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n; /* Declare variable n of type int */
+
+	for (n =0; n < 10; n++) /* Sets n to 0, increases upto 9 */
+	{
+		printf("%d", n); /* Prints current value of n */
+	}
+
+	printf("\n"); /* adds newline after loop ends */
+	return (0);
+}
