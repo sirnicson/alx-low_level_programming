720cc12b1e320fb3d3000465a5ab498290870bd2
Nicholas W <sirnicson@example.com>
2024-06-23 09:43:55 +0100

    Smile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry of the program
+ * ...
+ * Description: Print alphabets in  lowercase and in reverse
+ * Followed by a new line
+ * ...
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch; /* Declare variable ch of type char */
+	
+	for (ch = 'z'; ch >= 'a'; ch--) /* Sets ch = z, decreases to a */
+	{
+		putchar(ch); /* Prints ch in reverse (decrement) mode */
+	}
+	
+	putchar('\n'); /* Print new line after loop*/
+	return (0);
+}
