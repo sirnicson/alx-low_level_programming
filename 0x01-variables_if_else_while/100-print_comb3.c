a2927f043404e309f114dbf09d7f91ddd4bb58e4
Nicholas W <sirnicson@example.com>
2024-06-23 13:52:47 +0100

    Invention

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,21 +5,22 @@
  *
  * Description: Prints the smallest combination of two digits
  * Should be printed in ascending order
- * Combinations should be different 
+ * Combinations should be different
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int e, i;
+	
 	for (e = 0; e < 10; e++) /* Loop from 0 to 9 for e */
 	{
 		for (i = 0; i < 10; i++) /* Loop from 0 to 9 for i */
 		{
 			if (e < i) /* Ensure the first digit is less than the second */
 			{
-				putchar(e + '0'); /* Print digit e */
-				putchar(i + '0'); /* Print digit i */
+				putchar(e + '0');
+				putchar(i + '0');
 				if (!(e == 8 && i == 9)) /* Avoid printing comma and space after the last combination */
 				{
 					putchar(',');
