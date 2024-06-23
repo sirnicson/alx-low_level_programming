e106fec6cfd02fbc1663ae335abddb234fd59db8
Nicholas W <sirnicson@example.com>
2024-06-23 07:02:08 +0100

    Alphabet lowecase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
  * main - Entry point of the program
  * ..............
- * Description: Prints the last digit of the random number stored in the variable n
+ * Description: Prints alphabets in lover case,
+ * Followed by a new line
+ * ..............
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+	char ch;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d ", n, n % 10);
-	if (n % 10 > 5)
-		printf("and is greater than 5\n");
-	else if (n % 10 == 0)
-		printf("and is 0\n");
-	else if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6 and not 0\n");
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
 	return (0);
 }
