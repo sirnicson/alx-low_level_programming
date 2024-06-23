2469b6ebf46e11147c335d98e1b5b75345f34037
Nicholas W <sirnicson@example.com>
2024-06-23 13:49:20 +0100

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,33 +3,31 @@
 /**
  * main - Entry into the program
  *
- * Description: Prints possible combinations
- * of two digits numbers.
- * The two digits must be different
- * Should be printed in ascending order 
+ * Description: Prints the smallest combination of two digits
+ * Should be printed in ascending order
+ * Combinations should be different 
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int e, i; /*Declare variables e and i, of type int*/
-
-	for (e = 0; e < 10; e++) /*Loops from 0 to 9 for e */
+	int e, i;
+	for (e = 0; e < 10; e++) /* Loop from 0 to 9 for e */
 	{
-		for (i = 0; i < 10; i++) /*Loops from 0 to 9 for i*/
+		for (i = 0; i < 10; i++) /* Loop from 0 to 9 for i */
 		{
-			if (e < i) /*First digit less than second digit*/
+			if (e < i) /* Ensure the first digit is less than the second */
 			{
-				putchar(e + '0'); /*Prints e if e < i*/
-				putchar(i + '0'); /*Prints i if e < i */
-				if (e != 8 &&  i != 9)/*To avoid printing comma after the last combination*/
+				putchar(e + '0'); /* Print digit e */
+				putchar(i + '0'); /* Print digit i */
+				if (!(e == 8 && i == 9)) /* Avoid printing comma and space after the last combination */
 				{
-					putchar(',');/*Print spac*/
+					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
-		putchar('\n');/*Print newline at the end*/
-		return (0);
 	}
+	putchar('\n'); /* Print newline at the end */
+	return (0);
 }
