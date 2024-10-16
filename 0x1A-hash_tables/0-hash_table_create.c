3d301043d03718feabc1a7574e18703cbb09c89a
sirnicson <sirnicson.nicson@gmail.com>
2024-10-16 07:17:40 +0300

    Updated implementation of  task 0 & test file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,35 +7,32 @@
  * Return: a pointer to the newly created hash table,
  *         or NULL if it fails.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *hash_table;
     unsigned long int i;
 
-    /* Check if the size is valid */
     if (size == 0)
-        return (NULL);
+        return (NULL);  /* Check for invalid size */
 
     /* Allocate memory for the hash table structure */
     hash_table = malloc(sizeof(hash_table_t));
     if (hash_table == NULL)
-        return (NULL);
+        return (NULL);  /* Return NULL if allocation fails */
 
     /* Allocate memory for the array of pointers */
     hash_table->array = malloc(sizeof(hash_node_t *) * size);
     if (hash_table->array == NULL)
     {
-        free(hash_table);
-        return (NULL);
+        free(hash_table);  /* Free previously allocated memory */
+        return (NULL);  /* Return NULL if allocation fails */
     }
 
     /* Initialize each element of the array to NULL */
     for (i = 0; i < size; i++)
         hash_table->array[i] = NULL;
 
-    /* Set the size of the hash table */
-    hash_table->size = size;
+    hash_table->size = size;  /* Set the size of the hash table */
 
-    return (hash_table);
+    return (hash_table);  /* Return the created hash table */
 }
